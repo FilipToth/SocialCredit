1205c3c5d2968b332e2072acc0be806d1cba934a
Filip Toth <68369112+FilipToth@users.noreply.github.com>
2021-07-08 12:28:00 -0400

    Consume message analyzers

--- src/Events/message.ts
+++ src/Events/message.ts
@@ -1,11 +1,19 @@
 import { Command, Event } from "../Interfaces";
+import ContentAnalyzer from "../Analyzers/MessageContentAnalyzer";
 import { Message } from "discord.js";
 
 export const event: Event = {
     name: 'message',
     run: (client, message: Message) => {
-        if (message.author.bot || !message.guild || !message.content.startsWith(client.config.prefix))
+        if (message.author.bot || !message.guild)
             return;
+
+        if (!message.content.startsWith(client.config.prefix))
+        {
+            // analyze message contents
+            const analyzer = new ContentAnalyzer(message.content, message);
+            analyzer.analyzeAndRespond();
+        }     
 
         const args = message.content
             .slice(client.config.prefix.length)
